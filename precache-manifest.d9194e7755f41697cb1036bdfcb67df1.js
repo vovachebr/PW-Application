7c768f758d0f67c3114e0cd17c85ea9676e0ddca
vovachebr <vovachebr@mail.ru>
2020-06-23 13:29:11 +0300

    Updates

---
+++ precache-manifest.d9194e7755f41697cb1036bdfcb67df1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12955c15d24cdfc502932d2f4e2f2ccc",
+    "url": "/PW-Application/index.html"
+  },
+  {
+    "revision": "642212672288009984f8",
+    "url": "/PW-Application/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "afd4e2bbbd1eb7d0ee39",
+    "url": "/PW-Application/static/js/2.cb7965df.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/PW-Application/static/js/2.cb7965df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642212672288009984f8",
+    "url": "/PW-Application/static/js/main.b0b10a3e.chunk.js"
+  },
+  {
+    "revision": "6e70f5ac8a9bef0312ef",
+    "url": "/PW-Application/static/js/runtime-main.20f8e580.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/PW-Application/static/media/logo.ee7cd8ed.svg"
+  }
+]);
